5c5f80ce02811754f197bb69d2bddb40fbac72e1
Nikhilesh Tadiparthi <to.nikhilesh@outlook.com>
2016-02-28 21:03:38 -0800

    Sample App: Minor modifications to User model

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   
-  before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
   has_secure_password
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -51,7 +51,14 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
-    assert_not duplicate_user.valid?
+    assert duplicate_user.valid?
+  end
+  
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
   
   test "password should be present (nonblank)" do
