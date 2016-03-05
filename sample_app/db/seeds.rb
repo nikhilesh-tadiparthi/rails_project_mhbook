4098664538a1d039d843330f78e072628de486d4
Nikhilesh Tadiparthi <to.nikhilesh@outlook.com>
2016-03-04 21:41:11 -0800

    Sample App: Finish edit, update, index and destroy actions

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Nikhilesh Tadiparthi",
+  email: "to.nikhilesh@outlook.com",
+  password:              "p@ssw0rd",
+  password_confirmation: "p@ssw0rd",
+  admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
